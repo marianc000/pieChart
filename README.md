d5ae8f3bb6dc71f773af1fc1dfd70aae6d8b6b95
mcaikovs <marian.caikovski@gmail.com>
2021-12-22 17:25:54 +0100

    first

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# The most inappropriate use case for React
-Even with an auxiliary fully fledged library
+# Drawing arc, sectors and simple pie charts with SVG paths
+Without libraries
 
-Sample code for my post https://marian-caikovski.medium.com/the-most-inappropriate-use-case-for-react-65535e97ba48 comparing form with and without React
+Sample code for my post https://marian-caikovski.medium.com/the-most-inappropriate-use-case-for-react-65535e97ba48 
--- js/main.js
+++ js/main.js
@@ -8,11 +8,19 @@ function values() {
     return Array.from({ length: random(15) + 3 }, () => random(9) + 1);
 }
 
-onePieDiv.innerHTML = pie(100, 1) + pie(100, 1, 2) + pie(100, 1, 2, 3) + pie(100, 1, 2, 3, 4) + pie(100, 1, 2, 3, 4, 5)+ pie(100, 1, 2, 3, 4, 5,6)+ pie(100, 1, 2, 3, 4, 5,6,7);
+onePieDiv.innerHTML = pie(100, 1) + pie(100, 1, 2) + pie(100, 1, 2, 3) + pie(100, 1, 2, 3, 4) + pie(100, 1, 2, 3, 4, 5) + pie(100, 1, 2, 3, 4, 5, 6) + pie(100, 1, 2, 3, 4, 5, 6, 7);
 
 
-for (let i = 0; i < 1000; i++) {
-    const svg = pie(50, ...values());
-    manyPiesDiv.insertAdjacentHTML('beforeend', svg);
+function many() {
+    for (let i = 0; i < 100; i++) {
+        const svg = pie(50, ...values());
+        manyPiesDiv.insertAdjacentHTML('beforeend', svg);
+    }
 }
-[...manyPiesDiv.querySelectorAll('svg')].forEach(el => el.style.transform = 'rotate(' + random(360) + 'deg)');
+
+for (let i = 0; i < 100; i++) {
+    setTimeout(many);
+}
+
+setTimeout(() =>
+    [...manyPiesDiv.querySelectorAll('svg')].forEach(el => el.style.transform = 'rotate(' + random(360) + 'deg)'));
